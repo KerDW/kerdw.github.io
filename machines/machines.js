caef32243afbd3d950d812525523809f220685dc
Álvaro Zamora <alvarozajim@gmail.com>
2026-01-28 01:35:24 +0100

    remove poorly done machines and add epsilon

--- machines/machines.js
+++ machines/machines.js
@@ -13,77 +13,6 @@ const MACHINES_DATA = [
         "folder": "editor"
     },
     {
-        "name": "browsed",
-        "platform": "htb",
-        "difficulty": "medium",
-        "techniques": [
-            "Chrome Addon Data Exfiltration",
-            "Arithmetic Expansion RCE",
-            "PyCache Poisoning",
-            "SUID Privilege Escalation"
-        ],
-        "folder": "browsed",
-        "status": "active"
-    },
-    {
-        "name": "agile",
-        "platform": "htb",
-        "difficulty": "medium",
-        "techniques": [
-            "LFI",
-            "Werkzeug PIN Exploit",
-            "Selenium Hijacking",
-            "Sudoedit CVE-2023-22809"
-        ],
-        "folder": "agile"
-    },
-    {
-        "name": "ambassador",
-        "platform": "htb",
-        "difficulty": "medium",
-        "techniques": [
-            "Grafana CVE-2021-43798",
-            "Git History Leak",
-            "Consul RCE"
-        ],
-        "folder": "ambassador"
-    },
-    {
-        "name": "amor",
-        "platform": "dockerlabs",
-        "difficulty": "easy",
-        "techniques": [
-            "SSH Brute Force",
-            "Steganography",
-            "Ruby Sudo Exploitation"
-        ],
-        "folder": "amor"
-    },
-    {
-        "name": "artificial",
-        "platform": "htb",
-        "difficulty": "easy",
-        "techniques": [
-            "TensorFlow H5 RCE",
-            "SQLite Credential Leak",
-            "Backrest RCE"
-        ],
-        "folder": "artificial"
-    },
-    {
-        "name": "backend",
-        "platform": "dockerlabs",
-        "difficulty": "easy",
-        "techniques": [
-            "Blind SQLi",
-            "SQLMap",
-            "SUID Grep",
-            "MD5 Crack"
-        ],
-        "folder": "backend"
-    }
-    ,
-    {
         "name": "pollution",
         "platform": "htb",
         "difficulty": "hard",
@@ -96,7 +25,7 @@ const MACHINES_DATA = [
         ],
         "folder": "pollution"
     }
-,
+    ,
     {
         "name": "meta",
         "platform": "htb",
@@ -107,5 +36,19 @@ const MACHINES_DATA = [
             "sudo env_keep abuse + gtfobins"
         ],
         "folder": "meta"
+    }
+    ,
+    {
+        "name": "epsilon",
+        "platform": "htb",
+        "difficulty": "medium",
+        "techniques": [
+            ".git credentials+code exposure",
+            "aws lambda credentials exposure",
+            "JWT forging",
+            "filtered SSTI",
+            "script tar symlink abuse"
+        ],
+        "folder": "epsilon"
     }
 ];
